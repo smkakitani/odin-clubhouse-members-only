40cb6e7771bba09a66f5096319b164c719b8cd60
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-11-07 14:46:22 -0300

    create table for users and messages

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,7 +5,26 @@ const { Client } = require("pg");
 
 
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  firstName VARCHAR ( 255 ),
+  lastName VARCHAR ( 255 ),
+  email VARCHAR ( 255 ),
+  password VARCHAR ( 255 ),
+  isMembership BOOLEAN DEFAULT FALSE,
+  isAdmin BOOLEAN DEFAULT FALSE,
+  signInDate DATE CURRENT_DATE,
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER REFERENCES users ON DELETE CASCADE,
+  title VARCHAR ( 255 ),
+  text VARCHAR ( 500 ),
+  timestamp TIMESTAMP WITH TIME ZONE CURRENT_TIMESTAMP
+);
+`;
 
 
 
