d02bb1428dc1a756ea1e0c3ac9594250d64c7fe7
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-11-09 23:25:18 -0300

    fix wrong variable name

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require("pg");
 
 // Connection with DB
 module.exports = new Pool({
-  connectionString: process.env.CONNECTION_STRING_LOCAL_DB || process.env.DATABASE_URL
+  connectionString: process.env.CONNECTION_LOCAL_DB || process.env.DATABASE_URL
 });
